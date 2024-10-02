98d569049b7e12279f3915e5165239ecb7edb68c
Lukita5 <819011@unizar.es>
2024-10-02 19:33:34 +0200

    fuentes aplicadas
    se ha añadido la fuente geist al texto de la pantalla inicial

--- frontend-DataHand/app/page.js
+++ frontend-DataHand/app/page.js
@@ -15,7 +15,7 @@ export default function Login() {
 
             {/* Contenedor de la izquierda */}
             <div className="relative z-10 flex flex-col items-center justify-center w-1/2 p-8">
-                <h1 className="text-5xl font-bold mb-4 text-white">DataHand</h1>
+                <h1 className="text-5xl font-bold mb-4 text-white" style={{ fontFamily: 'var(--font-geist-sans)' }}>DataHand</h1>
                 <Image
                     src="/images/logo.png"
                     alt="Logo"
@@ -23,7 +23,7 @@ export default function Login() {
                     height={250}
                     className="mb-4"
                 />
-                <p className="mb-6 text-gray-100 text-center text-lg">
+                <p className="mb-6 text-gray-100 text-center text-lg" style={{ fontFamily: 'var(--font-geist-sans)' }}>
                     Convierte datos en decisiones.<br />¡Eleva el rendimiento de tu equipo en cada jugada!
                 </p>
             </div>
@@ -31,30 +31,34 @@ export default function Login() {
             {/* Contenedor de la derecha */}
             <div className="relative z-10 flex items-center justify-center w-1/2 p-8">
                 <div className="w-full max-w-md">
-                    <h2 className="text-3xl font-semibold text-center mb-6 text-white">Iniciar Sesión</h2>
+                    <h2 className="text-3xl font-semibold text-center mb-6 text-white" style={{ fontFamily: 'var(--font-geist-sans)' }}>
+                        Iniciar Sesión
+                    </h2>
                     <form className="flex flex-col gap-6">
                         <input
                             type="text"
                             placeholder="Nombre de usuario"
-                            className="bg-transparent border-b-2 border-white text-purple-600 placeholder-gray-200 p-3 focus:outline-none focus:border-purple-600 transition-all duration-300"
+                            className="input-field"
+                            style={{fontFamily: 'var(--font-geist-mono)'}}
                         />
                         <input
                             type="password"
                             placeholder="Contraseña"
-                            className="bg-transparent border-b-2 border-white text-purple-600 placeholder-gray-200 p-3 focus:outline-none focus:border-purple-600 transition-all duration-300"
+                            className="input-field"
+                            style={{fontFamily: 'var(--font-geist-mono)'}}
                         />
                         <Link href="/home">
-                            <button className="bg-transparent text-white border-2 border-white p-3 rounded-full w-full font-semibold hover:bg-white hover:text-purple-600 transition duration-300 ease-in-out text-center">
+                            <button className="bg-transparent text-white border-2 border-white p-3 rounded-full w-full font-semibold hover:bg-white hover:text-purple-600 transition duration-300 ease-in-out text-center" style={{ fontFamily: 'var(--font-geist-sans)' }}>
                                 Iniciar Sesión
                             </button>
                         </Link>
                         <Link href="/register">
-                            <button className="bg-transparent text-white border-2 border-white p-3 rounded-full w-full font-semibold hover:bg-white hover:text-purple-600 transition duration-300 ease-in-out text-center">
+                            <button className="bg-transparent text-white border-2 border-white p-3 rounded-full w-full font-semibold hover:bg-white hover:text-purple-600 transition duration-300 ease-in-out text-center" style={{ fontFamily: 'var(--font-geist-sans)' }}>
                                 Registrarse
                             </button>
                         </Link>
                     </form>
-                    <p className="text-white text-center mt-6">
+                    <p className="text-white text-center mt-6" style={{ fontFamily: 'var(--font-geist-sans)' }}>
                         ¿Olvidaste tu contraseña?{" "}
                         <Link href="/forgot-password" className="text-purple-600 hover:text-white transition">
                             Recuperar
