e26572f93c77aaa03cc7d65df78f83e161631a12
Lukita5 <819011@unizar.es>
2024-10-04 12:43:23 +0200

    home hecho
    pantalla a la que se llevará a los usuarios que sean entrenador y jugador a la vez

--- frontend-DataHand/app/home/page.js
+++ frontend-DataHand/app/home/page.js
@@ -1,41 +1,89 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="relative h-screen flex flex-col items-center justify-center bg-orange-500 overflow-hidden">
+        <div
+            className="relative h-screen flex flex-col items-center justify-center bg-gradient-to-r from-orange-500 to-purple-500 overflow-hidden animate-gradient">
+
+            {/* Contenedor del texto con más espacio entre el texto y los botones */}
             <h1 className="text-5xl font-bold mb-4 text-white" style={{fontFamily: 'var(--font-geist-sans)'}}>
                 Bienvenido a DataHand
             </h1>
-            <p className="text-lg text-center text-gray-100 mb-8" style={{fontFamily: 'var(--font-geist-sans)'}}>
+            <p className="text-lg text-center text-gray-100 mb-20" style={{fontFamily: 'var(--font-geist-sans)'}}>
                 Elige una opción para continuar
             </p>
-            <div className="flex flex-col gap-4">
-                <Link href="/register-match">
-                    <button
-                        className="bg-transparent text-white border-2 border-white p-3 rounded-full w-full font-semibold hover:bg-white hover:text-purple-600 transition duration-300 ease-in-out text-center" style={{ fontFamily: 'var(--font-geist-sans)' }}>
-                        Registrar Partido
-                    </button>
-                </Link>
+
+            {/* Contenedor de botones Entrenador y Jugador */}
+            <div className="grid grid-cols-2 gap-6 w-3/4 mb-10"> {/* Espacio añadido */}
                 <Link href="/profile-entrenador">
                     <button
-                        className="bg-transparent text-white border-2 border-white p-3 rounded-full w-full font-semibold hover:bg-white hover:text-purple-600 transition duration-300 ease-in-out text-center" style={{ fontFamily: 'var(--font-geist-sans)' }}>
-                        Perfil Entrenador
+                        className="flex items-center justify-center bg-transparent text-white border-2 border-white py-10 px-5 text-4xl rounded-full w-full font-semibold hover:bg-white hover:text-purple-600 transition duration-300 ease-in-out text-center"
+                        style={{fontFamily: 'var(--font-geist-sans)'}}>
+                        <Image
+                            src="/images/bxs-user.svg"
+                            alt="Logo"
+                            width={50}
+                            height={50}
+                            className="mr-2"
+                        />
+                        Entrenador
                     </button>
                 </Link>
                 <Link href="/profile-jugador">
                     <button
-                        className="bg-transparent text-white border-2 border-white p-3 rounded-full w-full font-semibold hover:bg-white hover:text-purple-600 transition duration-300 ease-in-out text-center" style={{ fontFamily: 'var(--font-geist-sans)' }}>
-                        Perfil Jugador
+                        className="flex items-center justify-center bg-transparent text-white border-2 border-white py-10 px-5 text-4xl rounded-full w-full font-semibold hover:bg-white hover:text-purple-600 transition duration-300 ease-in-out text-center"
+                        style={{fontFamily: 'var(--font-geist-sans)'}}>
+                        <Image
+                            src="/images/bx-user.svg"
+                            alt="Logo"
+                            width={50}
+                            height={50}
+                            className="mr-2"
+                        />
+                        Jugador
                     </button>
                 </Link>
-                <Link href="/">
+            </div>
+
+            <div className="grid grid-cols-1 gap-6 w-2/4 mb-10"> {/* Espacio añadido */}
+                <Link href="/profile-entrenador">
                     <button
-                        className="bg-transparent text-white border-2 border-white p-3 rounded-full w-full font-semibold hover:bg-white hover:text-purple-600 transition duration-300 ease-in-out text-center" style={{ fontFamily: 'var(--font-geist-sans)' }}>
+                        className="flex items-center justify-center bg-transparent text-white border-2 border-white py-10 px-5 text-4xl rounded-full w-full font-semibold hover:bg-white hover:text-purple-600 transition duration-300 ease-in-out text-center"
+                        style={{fontFamily: 'var(--font-geist-sans)'}}>
+                        <Image
+                            src="/images/bx-power-off.svg"
+                            alt="Logo"
+                            width={50}
+                            height={50}
+                            className="mr-2"
+                        />
                         Salir
                     </button>
                 </Link>
             </div>
+
+            <style jsx>{`
+                @keyframes gradient {
+                    0% {
+                        background-position: 0% 50%;
+                    }
+                    50% {
+                        background-position: 100% 50%;
+                    }
+                    100% {
+                        background-position: 0% 50%;
+                    }
+                }
+
+                .animate-gradient {
+                    background: linear-gradient(270deg, #ffb835, #8A2BE2);
+                    background-size: 400% 400%;
+                    animation: gradient 20s ease infinite;
+                }
+            `}</style>
         </div>
     );
 }
--- frontend-DataHand/app/profile-entrenador/page.js
+++ frontend-DataHand/app/profile-entrenador/page.js
@@ -30,24 +30,27 @@ export default function Home() {
     };
 
     return (
-        <div className="relative h-screen flex flex-col items-center justify-center bg-orange-500 overflow-hidden">
-            <h1 className="text-5xl font-bold mb-4 text-white" style={{ fontFamily: 'var(--font-geist-sans)' }}>
+        <div
+            className="relative h-screen flex flex-col items-center justify-center bg-gradient-to-r from-orange-500 to-purple-500 overflow-hidden animate-gradient">
+            <h1 className="text-5xl font-bold mb-4 text-white" style={{fontFamily: 'var(--font-geist-sans)'}}>
                 Perfil Entrenador
             </h1>
 
             {/* Contenedor para los cuadrados grandes con borde redondeado */}
             <div className="flex justify-center gap-8 mb-12 flex-wrap">
                 {/* Primer cuadrado con contenido */}
-                <div className="w-[40vw] h-[40vw] max-w-[500px] max-h-[500px] bg-white rounded-2xl flex flex-col justify-between p-4">
+                <div
+                    className="w-[40vw] h-[40vw] max-w-[500px] max-h-[500px] bg-white rounded-2xl flex flex-col justify-between p-4">
                     {/* Parte superior: Nombre y Nacionalidad */}
                     <div className="flex justify-between">
-                        <p className="text-xl font-semibold text-orange-500">Alejandro Sanz</p> {/* 888:esto sera variable */}
+                        <p className="text-xl font-semibold text-orange-500">Alejandro
+                            Sanz</p> {/* 888:esto sera variable */}
                         <p className="text-xl font-semibold text-orange-500">ESP</p> {/* 888:esto sera variable */}
                     </div>
 
                     {/* Imagen centrada */}
                     <div className="flex justify-center items-center h-full">
-                        <Image 
+                        <Image
                             src="/image/logo.png" // Cambiar este path 
                             alt="Imagen predefinida"
                             width={150}
@@ -58,11 +61,13 @@ export default function Home() {
 
                     {/* Parte inferior: Nombre del equipo */}
                     <div className="flex justify-center">
-                        <p className="text-2xl font-bold text-orange-500">Balonmano Zaragoza</p> {/* 888:esto sera variable */}
+                        <p className="text-2xl font-bold text-orange-500">Balonmano
+                            Zaragoza</p> {/* 888:esto sera variable */}
                     </div>
                 </div>
-                <div className="w-[40vw] h-[40vw] max-w-[500px] max-h-[500px] bg-white rounded-2xl flex items-center justify-center">
-                    <Radar data={data} />
+                <div
+                    className="w-[40vw] h-[40vw] max-w-[500px] max-h-[500px] bg-white rounded-2xl flex items-center justify-center">
+                    <Radar data={data}/>
                 </div>
             </div>
 
@@ -70,7 +75,8 @@ export default function Home() {
             <div className="w-[85vw] max-w-[1048px] bg-gray-200 rounded-lg flex flex-col justify-center mb-12 p-4">
                 {/* Fila de partidos */}
                 {['Partido 1', 'Partido 2', 'Partido 3'].map((partido, index) => (
-                    <div key={index} className="flex justify-between items-center p-2 border-b border-gray-400 mb-2 bg-gray-300 rounded">
+                    <div key={index}
+                         className="flex justify-between items-center p-2 border-b border-gray-400 mb-2 bg-gray-300 rounded">
                         <p className="text-2xl text-orange-500 font-semibold">{partido}</p>
                         <div className="flex gap-2">
                             <button className="bg-blue-500 text-white px-4 py-2 rounded">Editar</button>
@@ -89,6 +95,24 @@ export default function Home() {
                     </button>
                 </Link>
             </div>
+            <style jsx>{`
+                @keyframes gradient {
+                    0% {
+                        background-position: 0% 50%;
+                    }
+                    50% {
+                        background-position: 100% 50%;
+                    }
+                    100% {
+                        background-position: 0% 50%;
+                    }
+                }
+                .animate-gradient {
+                    background: linear-gradient(270deg, #ffb835, #8A2BE2);
+                    background-size: 400% 400%;
+                    animation: gradient 20s ease infinite;
+                }
+            `}</style>
         </div>
     );
 }
