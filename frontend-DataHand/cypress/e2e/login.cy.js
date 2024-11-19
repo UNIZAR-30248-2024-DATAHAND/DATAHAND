78efa993d79446f64bc30649e9a443544eaa30ff
alexsanz66 <jandrolodon@gmail.com>
2024-11-19 23:45:50 +0100

    selenium

--- frontend-DataHand/cypress/e2e/login.cy.js
+++ frontend-DataHand/cypress/e2e/login.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000')
-  })
-})
+describe('Login Page Tests', () => {
+  it('should navigate to /home when clicking the login button', () => {
+    // Visita la página de inicio de sesión
+    cy.visit('http://localhost:3000/');
+
+    // Verifica que el botón de "Iniciar Sesión" está visible
+    cy.contains('button', 'Iniciar Sesión').should('be.visible');
+
+    // Haz clic en el botón
+    cy.contains('button', 'Iniciar Sesión').click();
+
+    // Verifica que la URL cambió a /home
+    cy.url().should('include', '/home');
+  });
+});
